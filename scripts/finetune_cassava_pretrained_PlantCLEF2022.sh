3ae7eab0ed50adfe5694eabb4f886d54932fa4db
Xu Mingle <xml94@qq.com>
2022-06-18 17:08:13 +0900

    cassava

--- scripts/finetune_cassava_pretrained_PlantCLEF2022.sh
+++ scripts/finetune_cassava_pretrained_PlantCLEF2022.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 ##########################
-#  PVD
+#  cassava
 ##########################
 export epoch=400
-export name="base_PVD_split_118_epoch400"
-export batch=128
-export IMAGENET_DIR="./../../datasets/PlantVillage_split_118"
-export PRETRAIN_CHKPT='./ckpt/checkpoint-38.pth'
+export dataset="train20_val20_test20"
+export name="cassava_PlantCLEF2022_${dataset}"
+export batch=32
+export IMAGENET_DIR="/home/oem/Mingle/datasets/cassava/${dataset}"
+export PRETRAIN_CHKPT='./ckpt/PlantCLEF2022_MAE_vit_large_patch16.pth'
 CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 main_finetune.py \
     --accum_iter 8 \
     --batch_size ${batch} \
@@ -16,7 +17,7 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 m
     --blr 5e-4 --layer_decay 0.65 \
     --weight_decay 0.05 --drop_path 0.1 --mixup 0.8 --cutmix 1.0 --reprob 0.25 \
     --dist_eval --data_path ${IMAGENET_DIR} \
-    --nb_classes 38 \
+    --nb_classes 5 \
     --output_dir checkpoint/${name} \
     --log_dir checkpoint/${name}/"log" \
-    --eval_epoch 50
+    --eval_epoch 5
